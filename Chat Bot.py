75929aefe7649cce831b64cdeda5af47a78502a2
IsfFajar <123297033+IsfFajar@users.noreply.github.com>
2023-03-07 00:56:26 +0700

    Update Chat Bot.py

--- Chat Bot.py
+++ Chat Bot.py
@@ -1,8 +1,17 @@
 #importing ChatBot
 from chatterbot import ChatBot
+#importing Listtrainer to train chatbot
+from chatterbot.trainers import ListTrainer
 
 #create instance chatbot
 chatbot = ChatBot("Chatpot")
+
+#create trainer
+trainer = ListTrainer(chatbot)
+trainer.train(["Hi", 
+               "Welcome, friend", ])
+trainer.train(["Are you a plant?", 
+               "No, I'm the pot below the plant!", ])
 
 #create exit conditions to stop "whileloop"
 exit_conditions = (":q", "quit", "exit")
